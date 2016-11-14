4ae019f6686148f8cc709dfec3566d167523f6d7
npraveen35 <npraveen35@gmail.com>
2016-11-14 15:14:59 +0530

    updated function of fibonacci

--- basic_codes/fibonacci.py
+++ basic_codes/fibonacci.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 #V1
+def fib(n):
+    a,b=0,1
+    i =1
+    while i<=n:
+        print a,
+        a,b=b,a+b
+        i+=1
 
 n = input('enter number:')
-a,b=0,1
-i =1
-while i<=n:
-    print a,
-    a,b=b,a+b
-    i+=1
+fib(n)
