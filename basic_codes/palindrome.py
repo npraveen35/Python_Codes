21e914f48de1efcf192a518b28a49a66fffca5e8
npraveen35 <npraveen35@gmail.com>
2016-11-14 15:31:20 +0530

    function for palindrome

--- basic_codes/palindrome.py
+++ basic_codes/palindrome.py
@@ -1,6 +1,12 @@
-my_str = raw_input('Enter your string:')
-rev_str = my_str[::-1]
-if my_str == rev_str:
-    print my_str, "is PALINDROME"
-else:
-    print my_str, 'is NOT A PALINDROME'
+#!/usr/bin/python
+
+str = raw_input('Enter your string:')
+
+def palindrome_check(my_str):
+    rev_str = my_str[::-1]
+    if my_str == rev_str:
+        print my_str, "is PALINDROME"
+    else:
+        print my_str, 'is NOT A PALINDROME'
+
+palindrome_check(str)
