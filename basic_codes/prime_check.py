b2fa288c1d1b9e05b5f9fbcef5f63eff3765cd09
npraveen35 <npraveen35@gmail.com>
2016-11-14 15:36:01 +0530

    function for prime number checking

--- basic_codes/prime_check.py
+++ basic_codes/prime_check.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
-n = int(raw_input('enter your number:'))
-i = 2
-if (n == 1) | (n == 0):
-    print n,'is not PRIME\n'
+num = int(raw_input('enter your number:'))
+def prime_chk(n):
+    i = 2
+    if (n == 1) | (n == 0):
+        print n,'is not PRIME\n'
 
-else:
-    while i <= n-1:
-        if n % i == 0:
-            print "NOT A PRIME." #COMPOSITE NUMBER
-            exit()
-        i=i+1
-    print 'Number is PRIME'
+    else:
+        while i <= n-1:
+            if n % i == 0:
+                print "NOT A PRIME." #COMPOSITE NUMBER
+                exit()
+            i=i+1
+        print 'Number is PRIME'
+
+prime_chk(num)
