a8e57ad040242b2096f51aa7d284a665db46f850
npraveen35 <npraveen35@gmail.com>
2016-11-18 13:08:06 +0530

    update palindrome function

--- basic_codes/palindrome.py
+++ basic_codes/palindrome.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+#V1
 str = raw_input('Enter your string:')
 
 def palindrome_check(my_str):
@@ -10,3 +10,9 @@ def palindrome_check(my_str):
         print my_str, 'is NOT A PALINDROME'
 
 palindrome_check(str)
+
+#V2
+'''
+def palindrome(str):
+    return str == str[::-1]
+'''
