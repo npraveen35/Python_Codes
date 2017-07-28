61ea5a1b02cbff33fc8619520667053de82d1016
Praveen N <npraveen35@users.noreply.github.com>
2017-07-28 12:32:15 +0530

    fibonaci series good version
    Good Version V3 added

--- basic_codes/fibonacci.py
+++ basic_codes/fibonacci.py
@@ -24,3 +24,18 @@ def fibon(lim):
 n = input('enter number:')
 fibon(n)
 '''
+
+#V3 Displays the first n numbers in fibonacci series [ GOOD VERSION ]
+'''
+#!/usr/bin/python
+def fibo(n):
+    a,b=0,1
+    print a,b,
+    for i in range(3, n+1):
+        a,b = b, a+b
+        print b,
+
+Example Output:
+fibo(8)
+0 1 1 2 3 5 8 13
+'''
