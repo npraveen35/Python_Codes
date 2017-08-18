de331d804a0edb9cecaf252907d4e5df18d13c32
Praveen N <npraveen35@users.noreply.github.com>
2017-08-18 16:58:22 +0530

    palindrome V2 updated
    check palindrome of a sentence

--- basic_codes/palindrome.py
+++ basic_codes/palindrome.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-#V1
+# V1 checks for word
 
 def reverse(text):
     return text[::-1]
@@ -14,4 +14,18 @@ if is_palindrome(my_str.lower()):
 else:
     print my_str, 'is NOT A PALINDROME'
 
-#V2
+# V2 check the statement
+text = "Rise to vote, sir."
+original_text=''.join(t for t in text if t.isalnum()) # joins the each character continuously eliminating special char
+#print original_text
+
+def reverse(text):
+    return text[::-1]
+
+def is_palindrome(text):
+    return text == text[::-1]
+
+if is_palindrome(original_text.lower()):
+    print 'Yes, its palindrome'
+else:
+    print 'No, its not palindrome'
